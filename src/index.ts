2ce634112c3d7e5d2d3ee504241afe64713f21f0
Charles <adeakinyemi129@gmail.com>
2023-11-14 23:34:55 +0100

    tested post request

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import compression from "compression"
 import mongoose from "mongoose"
+import router from "./router"
 
 
 const app = express()
@@ -17,17 +18,19 @@ app.use(cors({
 app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
+app.use('/', router())
 
 
 const server = http.createServer(app)
 
-server.listen(3000, ()=> {
-    console.log("listening on port 3000 ...")
-})
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/"
+const MONGO_URL = "mongodb://127.0.0.1:27017/charlesDB"
 
 mongoose.Promise = Promise;
 
 mongoose.connect(MONGO_URL)
 mongoose.connection.on("error", (error: Error) => console.log(error))
+
+server.listen(3000, ()=> {
+    console.log("listening on port 3000 ...")
+})
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,15 @@
 import express from "express"
 
+import authentication from "./authentication"
+
 const router = express.Router()
 
 export default (): express.Router => {
+    authentication(router)
+
     return router
 }
+
+// export default (router: express.Router) => {
+//     return router
+// }
