7c37c212060e9b01c135b6a5e0e766034d6487e7
Charles <adeakinyemi129@gmail.com>
2023-11-20 22:10:59 +0100

    update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { deleteUserById, getUser} from "../db/users"
+import { deleteUserById, getUser, getUserById} from "../db/users"
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -29,6 +29,19 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 
   export const updateUser = async (req: express.Request, res: express.Response) => {
     try {
+        const {id} = req.params
+        const {username} = req.body
+
+        if(!username){
+            return res.sendStatus(400)
+        }
+
+        const user = await getUserById(id)
+
+        user.username = username
+        await user.save()
+
+        return res.status(200).json(user).end
         
     } catch (error) {
         console.log(error);
